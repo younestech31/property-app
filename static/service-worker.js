54f13d64f7f992a03efba5e206f0eef0244bd8d8
younestech31 <younesrahal2016@gmail.com>
2026-03-28 09:55:06 +0100

    Add PWA support: manifest, service worker, icons

---
+++ static/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'oran-real-estate-v1';
+const urlsToCache = [
+  '/',
+  '/static/icon-192x192.png',
+  '/static/icon-512x512.png',
+  '/static/Coat_Arms_Oran.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
